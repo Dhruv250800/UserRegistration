e375c52c7cbe319e7bd6a789f0e04f737a90be2a
DhruvGour <dhruv250800@gmail.com>
2023-06-12 20:01:08 +0530

    [DhruvGour]-adding UC1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UserRegistration;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Use Case 1: Validating First Name
+        string firstName = Console.ReadLine();
+        FirstNameValidator firstNameValidator = new FirstNameValidator();
+        if (firstNameValidator.ValidateFirstName(firstName))
+        {
+            Console.WriteLine("Valid first name");
+        }
+        else
+        {
+            Console.WriteLine("Invalid first name");
+        }
+    }
+}
---
+++ UC1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class FirstNameValidator
+    {
+        public bool ValidateFirstName(string firstName)
+        {
+            string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+            return Regex.IsMatch(firstName, pattern);
+        }
+    }
+}
